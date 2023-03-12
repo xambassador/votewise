db78c14f5c5b20599db6467defc6e90559b6dee1
Yash Ramoliya <yashramoliya12@gmail.com>
2023-03-12 11:58:10 +0530

    move routes to lib package

---
+++ packages/lib/logger.ts
@@ -0,0 +1,49 @@
+import dotenv from "dotenv";
+import pino from "pino";
+
+dotenv.config();
+
+const pinoInit = pino({
+  level: process.env.LOG_LEVEL || "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      translateTime: "SYS:standard",
+      ignore: "pid,hostname",
+      colorize: true,
+      crlf: true,
+      levelFirst: true,
+      messageFormat: false,
+      messageKey: "message",
+      timestampKey: "time",
+      base: null,
+      search: null,
+    },
+  },
+});
+
+const logger = (text: unknown, level = "info") => {
+  const isTestEnv = process.env.NODE_ENV === "test";
+  if (isTestEnv) {
+    return;
+  }
+  switch (level) {
+    case "info":
+      pinoInit.info(text);
+      break;
+    case "error":
+      pinoInit.error(text);
+      break;
+    case "warn":
+      pinoInit.warn(text);
+      break;
+    case "debug":
+      pinoInit.debug(text);
+      break;
+    default:
+      pinoInit.info(text);
+      break;
+  }
+};
+
+export { logger };
