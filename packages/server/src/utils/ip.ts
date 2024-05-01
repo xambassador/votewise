9e5ebd69dc67c18f309ea4c03b05c95470ed9703
Yash Ramoliya <yashramoliya12@gmail.com>
2024-05-01 22:19:28 +0530

    chore: remove server package

--- packages/server/src/utils/ip.ts
+++
@@ -1,8 +0,0 @@
-import type { Request } from "express";
-
-import ip from "request-ip";
-
-/** Get ip address from incomming request */
-export function getIp(req: Request) {
-  return ip.getClientIp(req);
-}
--- packages/server/src/utils/pagination.ts
+++
@@ -1,8 +0,0 @@
-export function getPagination(total: number, limit: number, offset: number) {
-  return {
-    total,
-    limit,
-    next: offset + limit,
-    isLastPage: total <= offset + limit,
-  };
-}
