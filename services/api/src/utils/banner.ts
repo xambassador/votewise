c00a980167966d86f5231c74501c912e1c5dc9e8
Yash Ramoliya <yashramoliya12@gmail.com>
2024-11-06 22:12:20 +0530

    fix: eslint configuration

--- services/api/src/utils/banner.ts
+++ services/api/src/utils/banner.ts
@@ -1,7 +1,7 @@
-import figlet from "figlet";
+import { textSync } from "figlet";
 
 export function banner() {
-  const text = figlet.textSync("Votewise", {
+  const text = textSync("Votewise", {
     font: "Ghost"
   });
   // eslint-disable-next-line no-console
