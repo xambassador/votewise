febe09b0ba5f46054191dce7d704d8d542e50934
Yash Ramoliya <yashramoliya12@gmail.com>
2024-12-25 23:29:20 +0530

    feat(common): introduce crypto package
    - Add new crypto package `@votewise/crypto` to share
      encryption/decryption and other common crypto related operation
      utilities to other packages.
    - Add new `obfuscateEmail` function

--- packages/common/text/src/truncate-on-word.ts
+++ packages/common/text/src/truncate-on-word.ts
@@ -5,3 +5,20 @@ export function truncateOnWord(text: string, maxLength: number, ellipsis = true)
   if (ellipsis) truncatedText += "...";
   return truncatedText;
 }
+
+export function obfuscateEmail(email: string) {
+  const [username, domain] = email.split("@");
+  if (!username || !domain) return email;
+  const usernameLength = username.length;
+
+  if (usernameLength <= 2) {
+    return `${username[0]}***@${domain}`;
+  }
+
+  const threshold = Math.floor(usernameLength / 2);
+  const maxLength = 4;
+  if (threshold <= maxLength) {
+    return `${username.substring(0, threshold)}***@${domain}`;
+  }
+  return `${username.substring(0, maxLength)}***@${domain}`;
+}
