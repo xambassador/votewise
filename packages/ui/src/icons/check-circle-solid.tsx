2e55d6a697cfcde24ddbcf7e5814bbe0cee2a72d
Yash Ramoliya <yashramoliya12@gmail.com>
2024-12-25 23:27:20 +0530

    feat(ui): add toast component
    - Add new toast component
    - Add check and cross icons
    - Inroduce `spinner` prop in image picker component to show fallback
      spinner instead of default hardcoded image

---
+++ packages/ui/src/icons/check-circle-solid.tsx
@@ -0,0 +1,14 @@
+type Props = React.SVGProps<SVGSVGElement>;
+
+export function CheckCircleSolid(props: Props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" {...props}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm14.707-1.293a1 1 0 0 0-1.414-1.414L11 13.586l-2.293-2.293a1 1 0 0 0-1.414 1.414L9.586 15a2 2 0 0 0 2.828 0l4.293-4.293Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
