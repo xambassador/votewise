cc6fe1f5963730ccd8d631e57eb88f3989e82123
Yash Ramoliya <yashramoliya12@gmail.com>
2025-04-08 22:55:21 +0530

    refactor(client): utilize `@votewise/types` package

--- packages/client/src/services/onboard.ts
+++ packages/client/src/services/onboard.ts
@@ -1,13 +1,11 @@
+import type { GetOnboardedStatusResponse, GetTopicsResponse, OnboardResponse } from "@votewise/types";
 import type { Client } from "../client";
 import type { Client as ServerClient } from "../server";
-import type { TFetchResult } from "../types";
 
 type OnboardOptions = {
   client: Client | ServerClient;
 };
 
-type GetOnboardedStatusResponse = { is_onboarded: boolean };
-type OnboardResponse = { is_onboarded: boolean };
 type OnboardBody = {
   user_name: string;
   first_name: string;
@@ -22,7 +20,6 @@ type OnboardBody = {
   twitter_url?: string | undefined;
   topics?: string[] | undefined;
 };
-type GetTopicsResponse = { topics: { id: string; name: string }[] };
 
 export class Onboard {
   private readonly client: Client | ServerClient;
@@ -31,19 +28,19 @@ export class Onboard {
     this.client = opts.client;
   }
 
-  public async isOnboarded(userId: string): Promise<TFetchResult<GetOnboardedStatusResponse>> {
+  public async isOnboarded(userId: string) {
     const path = `/v1/users/${userId}/onboard`;
     const res = await this.client.get<GetOnboardedStatusResponse>(path);
     return res;
   }
 
-  public async onboard(userId: string, data: OnboardBody): Promise<TFetchResult<OnboardResponse>> {
+  public async onboard(userId: string, data: OnboardBody) {
     const path = `/v1/users/${userId}/onboard`;
     const res = await this.client.patch<OnboardResponse, OnboardBody>(path, data);
     return res;
   }
 
-  public async getTopics(): Promise<TFetchResult<GetTopicsResponse>> {
+  public async getTopics() {
     const path = `/v1/topics`;
     const res = await this.client.get<GetTopicsResponse>(path);
     return res;
