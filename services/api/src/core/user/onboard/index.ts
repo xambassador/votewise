709a436a4614bb10aeb88ba3fb1c8e5e036b9095
Yash Ramoliya <yashramoliya12@gmail.com>
2025-04-13 22:29:40 +0530

    feat(api): sending mail after onboard complete
    Sending welcome mail after onboard. Update welcome email template
    content and remove email verification stuff.

--- services/api/src/core/user/onboard/index.ts
+++ services/api/src/core/user/onboard/index.ts
@@ -4,12 +4,14 @@ import { ExceptionLayer } from "@/lib/exception-layer";
 import { Controller } from "./controller";
 
 export function onboardControllerFactory() {
-  const ctx = AppContext.getInjectionTokens(["assert", "repositories", "plugins"]);
+  const ctx = AppContext.getInjectionTokens(["assert", "repositories", "plugins", "queues", "config"]);
   const controller = new Controller({
     assert: ctx.assert,
     requestParser: ctx.plugins.requestParser,
     userRepository: ctx.repositories.user,
-    userInterestRepository: ctx.repositories.userInterest
+    userInterestRepository: ctx.repositories.userInterest,
+    taskQueue: ctx.queues.tasksQueue,
+    appUrl: ctx.config.appUrl
   });
   const exceptionLayer = new ExceptionLayer({ name: "onboard" });
   return exceptionLayer.catch(controller.handle.bind(controller));
