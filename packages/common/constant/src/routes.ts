ec1ea428e472544c2c96d13602c27ca168e08157
Yash Ramoliya <yashramoliya12@gmail.com>
2025-04-27 10:44:27 +0530

    feat(packages/constant): add new sharable routes config
    Add sharable routes config file to use across packages and application
    code.

---
+++ packages/common/constant/src/routes.ts
@@ -0,0 +1,90 @@
+export const auth = {
+  paths: {
+    register: (base: string) => base + "/auth/register",
+    verify: (base: string) => base + "/auth/verify",
+    signin: (base: string) => base + "/auth/signin",
+    refresh: (base: string) => base + "/auth/refresh",
+    forgotPassword: (base: string) => base + "/auth/forgot-password",
+    resetPassword: (base: string) => base + "/auth/reset-password",
+    logout: (base: string) => base + "/auth/logout",
+    factors: {
+      enroll: (base: string) => base + "/auth/factors/enroll",
+      challengeFactor: (base: string) => base + "/auth/factors/:factor_id/challenge",
+      verifyFactor: (base: string) => base + "/auth/factors/:factor_id/verify"
+    },
+    emailVerificationSession: (base: string) => base + "/auth/verify/:email"
+  },
+  runtime: {
+    register: (base: string) => base + "/auth/register",
+    verify: (base: string) => base + "/auth/verify",
+    signin: (base: string) => base + "/auth/signin",
+    refresh: (base: string) => base + "/auth/refresh",
+    forgotPassword: (base: string) => base + "/auth/forgot-password",
+    resetPassword: (base: string) => base + "/auth/reset-password",
+    logout: (base: string) => base + "/auth/logout",
+    factors: {
+      enroll: (base: string) => base + "/auth/factors/enroll",
+      challengeFactor: (base: string, factorId: string) => base + `/auth/factors/${factorId}/challenge`,
+      verifyFactor: (base: string, factorId: string) => base + `/auth/factors/${factorId}/verify`
+    },
+    emailVerificationSession: (base: string, email: string) => base + `/auth/verify/${email}`
+  }
+};
+
+export const user = {
+  paths: {
+    sessions: (base: string) => base + "/users/sessions",
+    onboard: {
+      getStatus: (base: string) => base + "/users/:user_id/onboard",
+      update: (base: string) => base + "/users/:user_id/onboard"
+    },
+    usernameExists: (base: string) => base + "/users/:username/exists"
+  },
+  runtime: {
+    sessions: (base: string) => base + "/users/sessions",
+    onboard: {
+      getStatus: (base: string, userId: string) => base + `/users/${userId}/onboard`,
+      update: (base: string, userId: string) => base + `/users/${userId}/onboard`
+    },
+    usernameExists: (base: string, username: string) => base + `/users/${username}/exists`
+  }
+};
+
+export const topics = {
+  paths: {
+    all: (base: string) => base + "/topics"
+  },
+  runtime: {
+    all: (base: string) => base + "/topics"
+  }
+};
+
+export const feeds = {
+  paths: {
+    all: (base: string) => base + "/feeds",
+    create: (base: string) => base + "/feeds"
+  },
+  runtime: {
+    all: (base: string) => base + "/feeds",
+    create: (base: string) => base + "/feeds"
+  }
+};
+
+export const upload = {
+  paths: {
+    handshake: (base: string) => base + "/upload/handshake",
+    upload: (base: string) => base + "/upload",
+    status: (base: string) => base + "/upload/:token/status",
+    delete: (base: string) => base + "/upload/:token",
+    getAvatars: (base: string) => base + "/upload/assets/avatars",
+    getBackgrounds: (base: string) => base + "/upload/assets/backgrounds"
+  },
+  runtime: {
+    handshake: (base: string) => base + "/upload/handshake",
+    upload: (base: string) => base + "/upload",
+    status: (base: string, token: string) => base + `/upload/${token}/status`,
+    delete: (base: string, token: string) => base + `/upload/${token}`,
+    getAvatars: (base: string) => base + "/upload/assets/avatars",
+    getBackgrounds: (base: string) => base + "/upload/assets/backgrounds"
+  }
+};
